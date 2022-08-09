e80ad02f28daa1235af875aa313e8a093ce72db1
Matheus Oliveira <matheus_0119@outlook.com>
2022-08-09 14:24:01 -0300

    First commit - project created

---
+++ src/main/java/com/workshopmongo/WorkshopmonogoApplication.java
@@ -0,0 +1,13 @@
+package com.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmonogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmonogoApplication.class, args);
+	}
+
+}
