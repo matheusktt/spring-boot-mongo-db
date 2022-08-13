743042d7012fd9be4699f359823f6ac9b1da37ae
Matheus Oliveira <matheus_0119@outlook.com>
2022-08-13 01:13:32 -0300

    User insertion with POST

--- src/main/java/com/workshopmongo/services/UserService.java
+++ src/main/java/com/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.workshopmongo.domain.User;
+import com.workshopmongo.dto.UserDTO;
 import com.workshopmongo.repository.UserRepository;
 import com.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -28,5 +29,13 @@ public class UserService {
 			throw new ObjectNotFoundException("Object not found");
 		}
 		return user;
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
